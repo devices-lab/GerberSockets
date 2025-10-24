2b5c59bf0a4c03ff786947fd9532458bd6d5b591
Aron Eggens <aroneggens@gmail.com>
2025-10-24 16:37:10 +0100

    Remove "generate-" from config files

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/generate-GerberSockets/", // for GitHub Pages
+  // base: "/GerberSockets/", // for GitHub Pages
   base: "/", // for gerbersockets.devices-lab.org
 
 });
